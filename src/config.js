ab9b8822a8ecc2f1a96f2e07d035c491227f4f01
Diego de Oliveira <dodiego@gmail.com>
2019-11-26 17:49:58 -0300

    initial commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config({
+  path: require('path').join(__dirname, '..', '.env')
+})
+const env = require('require-env')
+module.exports = {
+  hostname: env.require('HOST'),
+  port: env.require('PORT'),
+  connectionString: process.env.CONNECTION_STRING || 'localhost'
+}
