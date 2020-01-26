b99d997e78bc359c84b9a4223e32c17008b3f131
Diego de Oliveira <dodiego@gmail.com>
2020-01-26 15:38:30 -0200

    added missing files

---
+++ src/rbac.js
@@ -0,0 +1,28 @@
+module.exports = {
+  roles: {
+    users: {
+      can: [
+        {
+          name: '*',
+          operation: 'create',
+          when: ctx => ctx.node._label !== 'users'
+        },
+        {
+          name: '*',
+          operation: 'update',
+          when: ctx => ctx.user._id === ctx.node._id || ctx.node.user_id === ctx.user._id
+        },
+        {
+          name: '*',
+          operation: 'delete',
+          when: ctx => ctx.user._id === ctx.node.user_id
+        },
+        {
+          name: '*',
+          operation: 'read',
+          when: ctx => !ctx.node.deleted
+        }
+      ]
+    }
+  }
+}
---
+++ src/schema.js
@@ -0,0 +1,9 @@
+module.exports = {
+  _label: 'users',
+  interests: {
+    _label: 'user_interests',
+    category: {
+      _label: 'interest_categories'
+    }
+  }
+}
